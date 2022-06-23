69f35fdcf3719398110c497d6d2a0b7afb38588d
ffffffl <65761562+ffffffl@users.noreply.github.com>
2022-06-23 17:36:48 +0800

    Update topic-mdelling.py

--- topic-mdelling.py
+++ topic-mdelling.py
@@ -42,5 +42,40 @@ vectorizer = CountVectorizer(analyzer='word',
 x_counts = vectorizer.fit_transform(train_headlines_sentences);
 
 
+#Next, we set a TfIdf Transformer, and transform the counts with the model.
 transformer = TfidfTransformer(smooth_idf=False);
 x_tfidf = transformer.fit_transform(x_counts);
+
+# And now we normalize the TfIdf values to unit length for each row.
+xtfidf_norm = normalize(x_tfidf, norm='l1', axis=1)
+
+
+#And finally, obtain a NMF model, and fit it with the sentences.
+
+#obtain a NMF model.
+model = NMF(n_components=num_topics, init='nndsvd');
+
+
+#fit the model
+model.fit(xtfidf_norm)
+
+def get_nmf_topics(model, n_top_words):
+    
+    #the word ids obtained need to be reverse-mapped to the words so we can print the topic names.
+    feat_names = vectorizer.get_feature_names()
+    
+    word_dict = {};
+    for i in range(num_topics):
+        
+        #for each topic, obtain the largest values, and add the words they map to into the dictionary.
+        words_ids = model.components_[i].argsort()[:-20 - 1:-1]
+        words = [feat_names[key] for key in words_ids][:n_top_words]
+        word_dict['Topic # ' + '{:02d}'.format(i+1)] = words;
+    
+    df = pd.DataFrame(word_dict)
+    df.index.rename("Top Words \ Topics", inplace=True)
+    return df
+
+
+
+get_nmf_topics(model, 10)
